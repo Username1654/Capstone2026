25a7ae82d2f8404abc410a55e7889366dac21d69
Tyler Thorne <tthorn000@west-mec.org>
2026-05-11 17:27:37 -0700

    day-2-little-progress

---
+++ Bestest_Game_Ever/src/gameBoard.jsx
@@ -0,0 +1,22 @@
+import { useEffect } from "react";
+
+
+
+
+    
+
+function GameBoard({ enemies, projectile, players }) {
+    
+    useEffect(() => {
+        enemies.forEach(
+            (enemy) => {
+                enemy.draw(document.getElementById("canvas").getContext("2d"))
+            }
+        )
+
+    }, [enemies,projectile,players])
+    return(<canvas id="canvas" width="100vw" height="100vh" style={{background:"red"}}  ></canvas>)
+    
+}
+
+export default GameBoard
